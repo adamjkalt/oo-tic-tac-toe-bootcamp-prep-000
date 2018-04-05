acd3a8b964e1161cd339b50874186eede5793adf
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-05 17:32:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,4 +23,105 @@ class TicTacToe
   [2,4,6],
 ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    display_board(board)
+<<<<<<< HEAD
+    move(board, index, current_player(board))
+=======
+    move(board, index, current_player)
+>>>>>>> 8b43ba63dd6412cf01497524ab0a29965b888be5
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  turns = 0
+board.each do |position|
+  if position == "X" || position == "O"
+    turns +=1
+  end
+end
+turns
+end
+
+
+def current_player(board)
+  turn_count(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else turn_count(board) % 2 == 1
+    "O"
+end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
+    return combo
+  elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+    return combo
+  end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? {|space| space == "X" || space == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+  true
+else
+  false
+end
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    true
+else
+  false
+end
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |combo|
+    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
+    return "X"
+  elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+    return "O"
+  end
+  end
+  return nil
+end
 end
