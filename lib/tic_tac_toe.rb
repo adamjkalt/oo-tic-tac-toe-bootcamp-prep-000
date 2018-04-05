9c4b4572e6cabe40d92aed6493b8d11af4b9ea78
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-05 17:50:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def move(index, current_player)
   board[index] = current_player
 end
 
-def position_taken?(location)
+def position_taken?
   board[location] != " " && board[location] != ""
 end
 
